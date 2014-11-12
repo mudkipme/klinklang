404a3dece683145eb24ea3e7ec678bd0b6396116
mudkip <i@mudkip.me>
2014-11-13 01:13:03 +0800

    remove some requirejs code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,30 +1,19 @@
-define([
-  'zepto'
-  ,'underscore'
-  ,'backbone'
-  ,'router'
-  ,'views/topbar'
-  ,'foundation/foundation.topbar'
-  ,'foundation/foundation.forms'
-], function($, _, Backbone, Router, TopBar){
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var Router = require('./router');
+var TopBar = require('./views/topbar');
 
-  var initialize = function(){
-    $(function(){
-      var router = new Router;
-      var topBar = new TopBar;
+$(function(){
+  var router = new Router;
+  var topBar = new TopBar;
 
-      router.bind('route', function(route){
-        topBar.update(route);
-      });
-      
-      topBar.render();
-      $(document).foundation();
+  router.bind('route', function(route){
+    topBar.update(route);
+  });
+  
+  topBar.render();
+  $(document).foundation();
 
-      Backbone.history.start({pushState: true});
-    });
-  };
-
-  return {
-    initialize: initialize
-  };
+  Backbone.history.start({pushState: true});
 });
