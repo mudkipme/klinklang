94faf01fd1d65bfb3a0b8fbdcf5d5ae792ddb81a
mudkip <i@mudkip.me>
2016-09-03 07:15:15 +0800

    Bring back data initialization action

---
+++ app/lib/database.js
@@ -0,0 +1,5 @@
+import sqlite3 from 'sqlite3';
+import path from 'path';
+
+const db = new sqlite3.Database(path.join(__dirname, '/../../database/base.sqlite3'));
+export default db;
--- radar.js
+++ radar.js
@@ -1,1 +1,17 @@
 import program from 'commander';
+import { init } from './app/actions/init';
+
+program
+.command('init')
+.description('Init the Pokédex database.')
+.action(async function() {
+  try {
+    await init();
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+if (program.parse(process.argv).args.length === 0) {
+  program.help();
+}
