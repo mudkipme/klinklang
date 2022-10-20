a175b5895724819b5a1b87604c1ed61c70391586
Mudkip <i@mudkip.me>
2022-10-21 02:09:14 +0800

    fix: discord message

--- packages/klinklang-server/src/actions/discord.ts
+++ packages/klinklang-server/src/actions/discord.ts
@@ -1,10 +1,10 @@
 import { diContainer } from '@fastify/awilix'
-import { MessageCreateOptions, MessagePayload, TextChannel } from 'discord.js'
+import { APIEmbed, MessageCreateOptions, MessagePayload, TextChannel } from 'discord.js'
 import { WikiWorker } from './wiki'
 
 export interface DiscordMessageActionInput {
   channel: string
-  message: string | MessagePayload | MessageCreateOptions
+  message: string | MessagePayload | MessageCreateOptions | { embed: APIEmbed }
 }
 
 export interface DiscordMessageActionOutput {
@@ -21,6 +21,10 @@ export class DiscordMessageWorker extends WikiWorker<DiscordMessageAction> {
   public async process (): Promise<DiscordMessageActionOutput> {
     const channel = await diContainer.cradle.discordClient.channels.fetch(this.input.channel)
     if (channel instanceof TextChannel) {
+      if (typeof this.input.message === 'object' && 'embed' in this.input.message) {
+        const message = await channel.send({ embeds: [this.input.message.embed] })
+        return { id: message.id }
+      }
       const message = await channel.send(this.input.message)
       return {
         id: message.id
